955e6d7a433cb6b390087f25c17846f2cd13e47c
ChansereyGit <samnangchanserey@gmail.com>
2025-12-16 11:31:15 +0700

    Configure microservices without secrets

---
+++ account/src/main/java/com/serey/bank/account/dto/CustomerDetailDTO.java
@@ -0,0 +1,12 @@
+package com.serey.bank.account.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class CustomerDetailDTO {
+    private CustomerDTO customer;
+    private List<CardResponseDTO> cards;
+    private List<LoanResponseDTO> loans;
+}
---
+++ account/src/main/java/com/serey/bank/account/service/client/LoanFeignClient.java
@@ -0,0 +1,15 @@
+package com.serey.bank.account.service.client;
+
+import com.serey.bank.account.dto.LoanResponseDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "loan")
+public interface LoanFeignClient {
+
+    @GetMapping("/api/loans/{customerId}")
+    List<LoanResponseDTO> getLoanInfo(@PathVariable Long customerId);
+}
